853f2161641a49e6b55063930ce89a5c9073746e
Aaron Hope <aghopecode@gmail.com>
2022-10-06 22:38:18 -0700

    fixed vertical centering prices and button

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        fm: '1440px'
+      },
+      colors: {
+        primary: {
+          600: 'hsla(0, 0%, 100%, 0.6)',
+          750: 'hsla(0, 0%, 100%, 0.75)'
+        },
+        secondary: {
+          300: 'hsl(277, 64%, 61%)',
+          600: 'hsl(244, 38%, 16%)',
+          900: 'hsl(233, 47%, 7%)'
+        }
+      },
+      fontFamily: {
+        body: ['Inter'],
+        lexand: ['Lexend Deca']
+      }
+    },
   },
   plugins: [],
 }
